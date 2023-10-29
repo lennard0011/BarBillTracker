11bb5789fcc75d7cc334ef284329ac5d4d180adb
Lennard van der Plas <lennardplas@gmail.com>
2023-10-29 12:27:00 +0100

    feat(client): add new user

--- client/src/app/users/page.tsx
+++ client/src/app/users/page.tsx
@@ -2,14 +2,21 @@
 
 import { useState, useEffect } from "react";
 
-type User = {
-    id: string,
-    name: string,
+type newUser = {
+    firstname: string,
+    lastname: string,
     email: string,
 }
 
+type User = newUser & {id: string};
+
 export default function Users() {
     const [users, setUsers] = useState<User[]>([]);
+    const [newUser, setNewUser] = useState<newUser>({
+         firstname: "First name",
+         lastname: "Last name",
+         email: "email",
+    } );
 
     useEffect(() => {
         const fetchUserData = async () => {
@@ -17,10 +24,11 @@ export default function Users() {
                 method: "GET"
             })
             const usersDataExteral = await usersDataResponse.json()
-            const usersDataFormatted = usersDataExteral.map((userData: {_id: string, firstname: string, lastname: string, email: string, }) => {
+            const usersDataFormatted = usersDataExteral.map((userData: { _id: string, firstname: string, lastname: string, email: string, }) => {
                 const formattedUser: User = {
                     id: userData._id,
-                    name: `${userData.firstname} ${userData.lastname}`,
+                    firstname: userData.firstname,
+                    lastname: userData.lastname,
                     email: userData.email
                 }
                 return formattedUser;
@@ -35,26 +43,67 @@ export default function Users() {
     async function deleteUser(userId: string) {
         console.log(userId)
         const removedUserResponse = await fetch(`http://localhost:8000/people/${userId}`, {
-            method: "DELETE",            
+            method: "DELETE",
         })
         return removedUserResponse;
     }
 
     const usersTableContent = users.map((user, index) => {
         return <tr key={index}>
-            <td>{user.name}</td>
+            <td>{user.firstname}</td>
+            <td>{user.lastname}</td>
             <td>{user.email}</td>
             <td><button onClick={() => deleteUser(user.id)}>❌</button></td>
-            </tr>
+        </tr>
     });
 
     const usersTableHeaders = <tr>
-        <th>Name</th>
+        <th>Firstname</th>
+        <th>Lastname</th>
         <th>E-mail</th>
         <th>Delete</th>
     </tr>
 
+    function createUserForm() {
+        return <tr>
+            <td>
+                <label htmlFor="firstname">
+                    <input type="text" value={newUser.firstname} className="bg-neutral-700" id="firstame" onChange={(e) => {
+                        setNewUser((user) => {
+                            return {...user, firstname: e.target.value}
+                        })
+                    }} />
+                </label>
+            </td>
+            <td>
+                <label htmlFor="lastname">
+                    <input type="text" value={newUser.lastname} className="bg-neutral-700" id="lastname" onChange={(e) => {
+                        setNewUser(() => {
+                            newUser.lastname = e.target.value
+                            return newUser
+                        })
+                    }}/>
+                </label>
+            </td>
+            <td>
+                <label htmlFor="email">
+                    <input type="text" value={newUser.email} className="bg-neutral-700" id="email" onChange={(e) => {
+                        setNewUser(() => {
+                            newUser.email = e.target.value
+                            return newUser
+                        })
+                    }}/>
+                </label>
+            </td>
+            <td>
+                <label>
+                    <input type="submit" value="Submit" />
+                </label>
+            </td>
+        </tr>
+    }
+
     return <div>
-        {users.length > 0 && <table>{usersTableHeaders} {usersTableContent}</table>}
+        {users.length > 0 && <table className="border-separate">{usersTableHeaders} {usersTableContent} {createUserForm()}</table>}
     </div>;
 }
